d91e8ba2ff84ced16c86540eb25dff3406c6123e
Brian Downs <brian.downs@gmail.com>
2018-05-08 09:54:58 -0700

    redo files

--- emailage_test.go
+++ emailage_test.go
@@ -264,3 +264,36 @@ func Test_removeBOM(t *testing.T) {
 		})
 	}
 }
+
+func TestErrorCodeLookup(t *testing.T) {
+	type args struct {
+		code int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "invalid input",
+			args: args{
+				code: 400,
+			},
+			want: "Invalid input parameter. Error message should indicate which one",
+		},
+		{
+			name: "not found",
+			args: args{
+				code: 9999,
+			},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ErrorCodeLookup(tt.args.code); got != tt.want {
+				t.Errorf("ErrorCodeLookup() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
